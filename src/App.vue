e441fcd21b6a091aa18d4ab1f381d1cbdbb67677
zhangrujie <1468196979@qq.com>
2018-09-04 13:30:59 +0800

    seeSEEThree3333333333335342424

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="app">
         <div class="box">
-            <!--陈相儒你看得到吗？看不到的是傻逼，傻逼傻逼，水水水水是-->
+            <!--陈相儒你看得到吗？看不到的是傻逼，傻逼傻逼，水水在啧啧啧啧啧啧啧啧啧啧啧啧啧啧啧啧啧啧啧啧啧水水是-->
             <router-view name="headers"></router-view>
             <router-view class="view"></router-view>
         </div>
