6022bedba57ec34d379d5ebc6e9e9fa2bca33a47
Chenxiangru <chenxiangru1@126.com>
2018-09-14 10:37:18 +0800

    重复ing

--- src/libs/config.js
+++ src/libs/config.js
@@ -3,16 +3,16 @@ import {isAndroidOrIos} from './tools';
 const get_usr_info=resolve=>{
     if(isAndroidOrIos()==-1||/localhost/.test(location.href)){
         common_param = {
+            // token: "29b7c656-f264-4f1a-ba2a-58af2e77efad",
+            // token: "d74c3ac0-4b07-490d-9c8e-74c86449c419,fd1e37eb-b9a8-4744-908a-d32abd472eea",
             token: "",
-            // token: "d74c3ac0-4b07-490d-9c8e-74c86449c419",
-            // token: "fd1e37eb-b9a8-4744-908a-d32abd472eea",
             userId: "",
             mobile: ''
         };
     }else{
         try {
             if (!window.jsondata) {
-            //   setTimeout(function(){
+              setTimeout(function(){
                 var infos = iosObject.getUserInfo();
                 let userInfo = infos;
                 var json = eval('(' + userInfo + ')');
@@ -23,8 +23,8 @@ const get_usr_info=resolve=>{
                     mobile: json.phone
                 };
                 return common_param;
-            //   },500)
-            } else {
+              },500)
+            } else  {
                 return {
                     token: window.jsondata.token,
                     userId: window.jsondata.userId,
